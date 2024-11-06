96691ce698e736ec83616b8aadfb76962a74ba54
SamikoGTS <itusgx@gmail.com>
2024-11-06 22:49:48 +0800

    Update config.js

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -10,8 +10,8 @@ const CONFIG = {
   COMMERCE_TEXT_FOOTER_MENU_1: 'Product Center', // COMMERCE主题页脚左侧菜单标题1
   COMMERCE_TEXT_FOOTER_MENU_2: 'About US', // COMMERCE主题页脚左侧菜单标题2
 
-  COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
-  COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
+  COMMERCE_FOOTER_RIGHT_IMG_URL: 'https://www.shuida123.com/qrcode.png', // 显示页脚右侧的图片，通常放二维码
+  COMMERCE_FOOTER_RIGHT_TEXT: '扫码关注公众号，回复关键词可查询进度', // 页脚右侧图片下的文字描述
 
   COMMERCE_HOME_POSTS_COUNT: 9, // 首页展示商品数
   COMMERCE_CONTACT_WHATSAPP_SHOW: true, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
